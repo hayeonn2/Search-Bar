fc5084d054ae7aeec17a277dabcd50f9741e8f5e
hayoenn2 <dwd1205@naver.com>
2023-05-03 17:40:08 +0900

    [Setting] ESLint 린팅 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,63 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-}
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks", "jsx-a11y"],
+  rules: {
+    "no-console": "warn",
+    "react/prop-types": "off",
+    "react/button-has-type": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/self-closing-comp": [
+      "warn",
+      {
+        component: true,
+        html: false,
+      },
+    ],
+    "react/jsx-sort-props": [
+      "warn",
+      {
+        shorthandFirst: true,
+        callbacksLast: true,
+        noSortAlphabetically: false,
+        reservedFirst: true,
+        multiline: "last",
+      },
+    ],
+    "jsx-a11y/anchor-has-content": [
+      "warn",
+      {
+        components: ["Link"],
+      },
+    ],
+    "jsx-a11y/anchor-is-valid": [
+      "warn",
+      {
+        components: ["Link"],
+      },
+    ],
+  },
+};
