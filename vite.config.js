47067b323a9791fbd555782f8bc3aee393f85fd7
hayoenn2 <dwd1205@naver.com>
2023-05-03 15:38:05 +0900

    [init] Vite로 초기 환경설정하기

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: "localhost",
+    port: 3000,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+        },
+      },
+    },
+  },
+});
