4c7d3ee7dc38c5a804885a6ef08298e05b20cbf9
hayoenn2 <dwd1205@naver.com>
2023-05-05 17:06:16 +0900

    [Setting] prettier setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,57 +6,58 @@ module.exports = {
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react", "react-hooks", "jsx-a11y"],
+  plugins: ['react', 'react-hooks', 'jsx-a11y'],
   rules: {
-    "no-console": "warn",
-    "react/prop-types": "off",
-    "react/button-has-type": "warn",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "react/self-closing-comp": [
-      "warn",
+    'no-console': 'warn',
+    'react/prop-types': 'off',
+    'react/button-has-type': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/self-closing-comp': [
+      'warn',
       {
         component: true,
         html: false,
       },
     ],
-    "react/jsx-sort-props": [
-      "warn",
+    'react/jsx-sort-props': [
+      'warn',
       {
         shorthandFirst: true,
         callbacksLast: true,
         noSortAlphabetically: false,
         reservedFirst: true,
-        multiline: "last",
+        multiline: 'last',
       },
     ],
-    "jsx-a11y/anchor-has-content": [
-      "warn",
+    'jsx-a11y/anchor-has-content': [
+      'warn',
       {
-        components: ["Link"],
+        components: ['Link'],
       },
     ],
-    "jsx-a11y/anchor-is-valid": [
-      "warn",
+    'jsx-a11y/anchor-is-valid': [
+      'warn',
       {
-        components: ["Link"],
+        components: ['Link'],
       },
     ],
   },
